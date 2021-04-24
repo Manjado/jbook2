40df80158b36f6e1aa4f96ea86268c766f8cac34
malchimowicz <michal.alchimowicz@enovatis.pl>
2021-04-24 13:43:12 +0200

    parallel script own cli

---
+++ packages/cli/src/commands/serve.ts
@@ -0,0 +1,12 @@
+import { Command } from 'commander';
+
+export const serveCommand = new Command()
+  .command('serve [filename]')
+  .description('Open a file for editing')
+  .option('-p, --port <number>', 'port to run server on', '4005')
+  .action((filename = 'notebook.js', options) => {
+    console.log(filename, options);
+  });
+
+//square brackets are indicating to commander that is an optional value
+//angle brackets are indicating to commander that is an required value
