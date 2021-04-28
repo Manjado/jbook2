798276c77a2afe5c454a76ad2aaef983cc4d2a0b
malchimowicz <michal.alchimowicz@enovatis.pl>
2021-04-28 22:18:56 +0200

    error handling, proxy

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,11 +1,26 @@
+import path from 'path';
 import { Command } from 'commander';
+import { serve } from 'local-api';
 
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action((filename = 'notebook.js', options) => {
-    console.log(filename, options);
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`
+      );
+    } catch (err) {
+      if (err.code === 'EADDRINUSE') {
+        console.error('Port is in use. Try running on a diffrent port.');
+      } else {
+        console.log('Heres tje problem', err.message);
+      }
+      process.exit(1);
+    }
   });
 
 //square brackets are indicating to commander that is an optional value
