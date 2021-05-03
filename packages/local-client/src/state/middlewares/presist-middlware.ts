7b7411a418387bcd9a0d700fdb619122deec5f7c
malchimowicz <michal.alchimowicz@enovatis.pl>
2021-05-03 18:33:51 +0200

    debouncing

--- packages/local-client/src/state/middlewares/presist-middlware.ts
+++ packages/local-client/src/state/middlewares/presist-middlware.ts
@@ -1,17 +1,36 @@
 import { Dispatch } from 'redux';
 import { Action } from '../actions';
 import { ActionType } from '../action-types';
-import { saveCells } from '../action-creators'
+import { saveCells } from '../action-creators';
 import { RootState } from '../reducers';
 
-export const persisMiddlware = ({ dispatch, getState } : { dispatch: Dispatch<Action>, getState: () => RootState}) => {
+export const persisMiddlware = ({
+  dispatch,
+  getState,
+}: {
+  dispatch: Dispatch<Action>;
+  getState: () => RootState;
+}) => {
+  let timer: any;
   return (next: (action: Action) => void) => {
     return (action: Action) => {
       next(action);
 
-      if ([ActionType.MOVE_CELL, ActionType.UPDATE_CELL, ActionType.INSERT_CELL_AFTER, ActionType.DELETE_CELL].includes(action.type)) {
-        saveCells()(dispatch, getState)
+      if (
+        [
+          ActionType.MOVE_CELL,
+          ActionType.UPDATE_CELL,
+          ActionType.INSERT_CELL_AFTER,
+          ActionType.DELETE_CELL,
+        ].includes(action.type)
+      ) {
+        if (timer) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+          saveCells()(dispatch, getState);
+        }, 250);
       }
-    }
-  }
-}
+    };
+  };
+};
